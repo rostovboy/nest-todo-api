45153b0b71246ab595e0e1517e564b23d6171552
Alexey T <rostovboy@gmail.com>
2022-02-06 01:35:45 +0300

    Added config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), TodosModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot(),
+    TodosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
